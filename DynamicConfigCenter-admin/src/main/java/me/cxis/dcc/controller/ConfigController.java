6aa1aa9d151067921a0629a3306c8aa36fb5117d
dachengxi <dachengdaxi@gmail.com>
2020-04-30 18:36:13 +0800

    add swagger

--- DynamicConfigCenter-admin/src/main/java/me/cxis/dcc/controller/ConfigController.java
+++ DynamicConfigCenter-admin/src/main/java/me/cxis/dcc/controller/ConfigController.java
@@ -1,5 +1,9 @@
 package me.cxis.dcc.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import me.cxis.dcc.manager.ConfigManager;
 import me.cxis.dcc.model.ConfigVO;
 import me.cxis.dcc.result.Result;
 import org.slf4j.Logger;
@@ -9,16 +13,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
+@Api(tags = {"配置管理"})
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigController.class);
 
+    @Resource
+    private ConfigManager configManager;
+
+    @ApiOperation(value = "添加配置", notes = "添加配置和配置实例，同步新增Zookeeper节点")
+    @ApiImplicitParam(name = "config", value = "配置", dataType = "ConfigVO", paramType = "body", required = true)
     @PostMapping("/addConfig")
     public Result<Boolean> addConfig(@RequestBody ConfigVO config) {
-
-        return null;
+        // TODO 校验
+        Result<Boolean> result = new Result<>();
+        result.setModel(configManager.addConfig(config));
+        return result;
     }
 
 }
---
+++ DynamicConfigCenter-admin/src/main/java/me/cxis/dcc/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package me.cxis.dcc.exception;
+
+import me.cxis.dcc.result.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ServiceException.class)
+    public Result<Void> serviceException(ServiceException exception) {
+        Result<Void> result = new Result<>();
+        result.setSuccess(false);
+        result.setCode(exception.getExceptionCode());
+        result.setMsg(exception.getMessage());
+        result.setModel(null);
+        return result;
+    }
+}
