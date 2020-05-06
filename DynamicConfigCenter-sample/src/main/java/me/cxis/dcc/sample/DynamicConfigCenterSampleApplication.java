90a585b5c5d956493b9fb3d594881d064767c0a1
dachengxi <dachengdaxi@gmail.com>
2020-05-06 14:23:33 +0800

    add annotation EnableDCC

--- DynamicConfigCenter-sample/src/main/java/me/cxis/dcc/sample/DynamicConfigCenterSampleApplication.java
+++ DynamicConfigCenter-sample/src/main/java/me/cxis/dcc/sample/DynamicConfigCenterSampleApplication.java
@@ -1,11 +1,15 @@
 package me.cxis.dcc.sample;
 
+import me.cxis.dcc.spring.EnableDCC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(value = {"classpath:dcc-spring.xml"})
+// xml配置文件方式
+// @ImportResource(value = {"classpath:dcc-spring.xml"})
+// 注解方式启用
+@EnableDCC
 public class DynamicConfigCenterSampleApplication {
 
     public static void main(String[] args) {
